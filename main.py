a6e329866cfa97b4b519852b36417c6353ab8ad4
Pablo Rangel <pablorangel@gmail.com>
2024-03-31 15:44:06 -0300

    Initial commit

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import cv2
+import pafy
 
-thres = 0.5 # Threshold to detect object
+thres = 0.42 # Threshold to detect object
+url = 'https://www.youtube.com/watch?v=tWUIUDd4DgE'
+pafy.backend="internal"
+video = pafy.new(url)
+best = video.getbest(preftype="mp4")
 
-cap = cv2.VideoCapture('samples/military.mp4')
+#cap = cv2.VideoCapture(best.url)
+cap = cv2.VideoCapture('samples/test.mp4')
 cap.set(3,1280)
 cap.set(4,720)
 cap.set(10,70)
@@ -12,12 +18,14 @@ classFile = 'classifier/ship_types'
 with open(classFile,'rt') as f:
     classNames = f.read().rstrip('\n').split('\n')
 
-configPath = 'detector/detector.pbtxt'
+configClassifier = 'detector/detector.pbtxt'
+thres_detection = 0.6
+thres_classifier = 0.8
 classifier = 'classifier/model.pb'
 detector = 'detector/detector.pb'
 
 netClassifier = cv2.dnn.readNetFromTensorflow(classifier)
-netDetector = cv2.dnn_DetectionModel(detector,configPath)
+netDetector = cv2.dnn_DetectionModel(detector,configClassifier)
 netDetector.setInputSize(224,224)
 netDetector.setInputScale(1.0/ 127.5)
 netDetector.setInputMean((127.5, 127.5, 127.5))
@@ -29,7 +37,7 @@ while True:
     if len(classIds) != 0:
         for classId, confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
 
-            if classId == 9:
+            if classId == 9 and confidence > thres_detection:
                 x = box[0]
                 y = box[1]
                 w = box[2]
@@ -38,7 +46,6 @@ while True:
                 name = 'Unknown'
                 max_value = 0
 
-                # if w >= 224 and h >= 224:
                 vessel_img = img[y:y+h, x:x+w]
                 vessel_img = cv2.resize(vessel_img, (224, 224))
                 new_img = cv2.normalize(vessel_img, None, 0, 1.0, cv2.NORM_MINMAX, dtype=cv2.CV_32F)
@@ -50,7 +57,10 @@ while True:
                     if max_value < preds[0][i]:
                         max_index = i
                         max_value = preds[0][i]
-                name = classNames[max_index]
+                if max_value < thres_classifier:
+                    name = 'Unknown'
+                else:
+                    name = classNames[max_index]
                 cv2.rectangle(img, box, color=(0, 255, 0), thickness=2)
                 cv2.putText(img,name.upper(),(box[0]+10,box[1]+30),
                             cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
